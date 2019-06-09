53f843a76f3773906e0b91be8a6a4b51e6227c49
latalavera <jesusfernandez0586@gmail.com>
2019-06-09 13:30:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/fakebook")
+def fakebook():
+    return render_template("fakebook.html")
+
+@app.route("/portfolio/google")
+def google():
+    return render_template("google.html")
+
+
+if __name__ == '__main__':
+    app.run()
